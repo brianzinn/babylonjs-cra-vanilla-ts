b1532e382a1237ab051abe47178d12b1bf46a1c8
Brian Zinn <github@wakeskate.com>
2020-05-05 22:39:37 -0700

    default playground

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
+import SceneComponent from 'babylonjs-hook';
 import './App.css';
+import { Scene, FreeCamera, Vector3, HemisphericLight, MeshBuilder, Mesh } from '@babylonjs/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let box: Mesh | undefined;
+
+const onSceneReady = (scene: Scene) => {
+  console.log('scene ready:', scene);
+
+  // This creates and positions a free camera (non-mesh)
+  var camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
+
+  // This targets the camera to scene origin
+  camera.setTarget(Vector3.Zero());
+
+  const canvas: HTMLCanvasElement = scene.getEngine()!.getRenderingCanvas()!;
+
+  // This attaches the camera to the canvas
+  camera.attachControl(canvas, true);
+
+  // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+  var light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
+
+  // Default intensity is 1. Let's dim the light a small amount
+  light.intensity = 0.7;
+
+  // Our built-in 'box' shape.
+  box = MeshBuilder.CreateBox("box", {size: 2}, scene);
+
+  // Move the box upward 1/2 its height
+  box.position.y = 1;
+
+  // Our built-in 'ground' shape.
+  MeshBuilder.CreateGround("ground", {width: 6, height: 6}, scene);
 }
 
-export default App;
+const onRender = (scene: Scene) => {
+  if (box !== undefined) {
+    var deltaTimeInMillis = scene.getEngine().getDeltaTime();
+
+    const rpm = 10;
+    box.rotation.y += ((rpm / 60) * Math.PI * 2 * (deltaTimeInMillis / 1000));
+  }
+}
+
+export default () => (
+    <div>
+      <SceneComponent antialias onSceneReady={onSceneReady} onRender={onRender} id='my-canvas' />
+    </div>
+)
