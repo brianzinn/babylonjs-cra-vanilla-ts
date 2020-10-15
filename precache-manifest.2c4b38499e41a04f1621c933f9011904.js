fa719d76b473ac46e18f4f08d6fd1789dee1d844
Brian Zinn <github@wakeskate.com>
2020-10-14 21:42:19 -0700

    Updates

---
+++ precache-manifest.2c4b38499e41a04f1621c933f9011904.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce6c0faf6b4d788f5b7f3ec7eea6a0b7",
+    "url": "/babylonjs-cra-vanilla-ts/index.html"
+  },
+  {
+    "revision": "59d42e6ea32dac64c131",
+    "url": "/babylonjs-cra-vanilla-ts/static/css/main.704cc935.chunk.css"
+  },
+  {
+    "revision": "31bb61e79fc9f7da7e28",
+    "url": "/babylonjs-cra-vanilla-ts/static/js/2.0608b9df.chunk.js"
+  },
+  {
+    "revision": "4f4a12233651b0855fd74d4d32936871",
+    "url": "/babylonjs-cra-vanilla-ts/static/js/2.0608b9df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59d42e6ea32dac64c131",
+    "url": "/babylonjs-cra-vanilla-ts/static/js/main.7c0b73d0.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/babylonjs-cra-vanilla-ts/static/js/main.7c0b73d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f70ba0062610e41a017e",
+    "url": "/babylonjs-cra-vanilla-ts/static/js/runtime-main.61b56238.js"
+  }
+]);
